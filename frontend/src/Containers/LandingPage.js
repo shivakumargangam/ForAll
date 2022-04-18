975ffa90dfa57c2fb13afda62e28826c3bce9a1f
ManiSai26 <kamutalamanisai26@gmail.com>
2022-04-18 20:16:22 +0530

    cleanup

---
+++ frontend/src/Containers/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const LandingPage = () => {
+    return ( 
+        <div>
+            <h1> Welcome to 4all!</h1>
+        </div>
+     );
+}
+ 
+export default LandingPage;
