c51ae6643042df17bd17d2a69aac3909312d5dab
DESKTOP-ANQ9B4T <gangamshivakumar01.com>
2022-05-03 10:59:00 +0530

    Homepage

--- frontend/src/Containers/LandingPage.js
+++ frontend/src/Containers/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import LandingNavbar from '../components/LandingNavbar';
+import LandingHome from '../components/LandingHome'
 const LandingPage = () => {
     return ( 
         <div>
             <LandingNavbar/>
+            <LandingHome/>
         </div>
      );
 }
---
+++ frontend/src/components/LandingHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Stylesheets/LandingHome.css';
+const LandingHome = () => {
+    return ( 
+        <div className='LpHome'>
+            <h1>Save orphans</h1>
+        </div>
+
+     );
+}
+ 
+export default LandingHome;
