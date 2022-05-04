6e274b46c5c406c7d7d4f574a4876e884c598254
ManiSai26 <kamutalamanisai26@gmail.com>
2022-05-04 11:07:28 +0530

    home

--- frontend/src/components/LandingHome.js
+++ frontend/src/components/LandingHome.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../Stylesheets/LandingHome.css';
+import background from '../media/3.svg';
 const LandingHome = () => {
     return ( 
-        <div className='LpHome'>
-            <h1>Save orphans</h1>
+        <div className='LpHome' style={{ backgroundImage: `url(${background})`,backgroundRepeat  : 'no-repeat',backgroundSize: 'cover', }}>
+            <h1>Save</h1>
+            <h1>Orphans</h1>
         </div>
 
      );
