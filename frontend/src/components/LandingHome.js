a1d099de3bcac15b2ba5eb66ca055bff0b9eed7d
DESKTOP-ANQ9B4T <gangamshivakumar01.com>
2022-05-04 11:06:08 +0530

    backgroundPage

--- frontend/src/components/LandingHome.js
+++ frontend/src/components/LandingHome.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import '../Stylesheets/LandingHome.css';
+import background from '../media/2.jpg';
 const LandingHome = () => {
     return ( 
-        <div className='LpHome'>
-            <h1>Save orphans</h1>
-        </div>
+        <div>
+            <div className='LpHome' style={{ backgroundImage: `url(${background})`,
+                    height:'110vh',
+                    marginTop:'-70px',
+                    fontSize:'50px',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat', }}>
+                <h1>Save </h1>
+                <h1>Orphans</h1>
+            </div>
+            <div>
+                <h1>Donate and change the life</h1>
+            </div>
 
+        </div>
      );
 }
  
